96a9897287f34e5b3d7d67df00ccd82c77c0b690
arunvinu <arunkumarxworkz@gmail.com>
2021-12-21 18:35:55 +0545

    view8

---
+++ Thumb.java
@@ -0,0 +1,10 @@
+class Thumb
+{
+static String [ ]name ={ "arun","akshay","akarsh"};
+static String names = "anu";
+public static void main(String args[ ])
+{
+System.out.println(name[0]);
+System.out.println(names);
+}
+}
